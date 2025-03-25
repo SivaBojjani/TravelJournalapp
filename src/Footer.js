77d78879775345b59f7a42bda1b5543e94725cd7
SivaBojjani <sivabojjani15@gmail.com>
2025-03-25 17:24:04 +0530

    updated

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,55 @@
 import React from "react";
 import "./Footer.css";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa"; // Import icons
 
-export default function Footer({ setShowContactForm, isLoggedIn }) { // Accepting the props
+export default function Footer({ setShowContactForm, isLoggedIn }) {
   return (
     <footer className="footer">
-      <p>
-        {isLoggedIn && ( // Only show the menu if the user is logged in
-          <>
-            <button className="btn contact-form-btn" onClick={() => setShowContactForm(true)}>Contact Us</button> {/* Button to open contact form */}
-            About | Help |{" "}
-          </>
-        )}
-        <a href="https://www.linkedin.com/in/siva-bojjani" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{" "}
-        <a href="https://github.com/SivaBojjani" target="_blank" rel="noopener noreferrer">GitHub</a> |
-      </p>
+      <div className="footer-container">
+        <div className="footer-links">
+          {isLoggedIn && (
+            <>
+              <button
+                className="btn contact-form-btn"
+                onClick={() => setShowContactForm(true)}
+              >
+                Contact Us
+              </button>
+              <span>About | Help | </span>
+            </>
+          )}
+          <a
+            href="https://www.linkedin.com/in/siva-bojjani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin /> LinkedIn
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://github.com/SivaBojjani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub /> GitHub
+          </a>
+        </div>
+
+        {/* Newsletter Signup Section */}
+        <div className="newsletter">
+          <p>Subscribe for travel updates:</p>
+          <form>
+            <input type="email" placeholder="Enter your email" required />
+            <button type="submit">
+              <FaEnvelope /> Subscribe
+            </button>
+          </form>
+        </div>
+
+        <p className="copyright">
+          © 2025 Travel Journal. All Rights Reserved.
+        </p>
+      </div>
     </footer>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,29 @@
-import React from "react"; // Importing React
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaHome, FaSearch, FaCog, FaInfoCircle, FaMap, FaUser } from "react-icons/fa";
 import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <a className="active" href="/"><i className="fa fa-fw fa-home"></i> Home</a>
-      <a href="/search"><i className="fa fa-fw fa-search"></i> Search</a>
-      <a href="/contact"><i className="fa fa-fw fa-envelope"></i> Contact</a>
-      <a href="/settings"><i className="fa fa-fw fa-cog"></i> Settings</a> {/* New menu item */}
-      <a href="/about"><i className="fa fa-fw fa-info"></i> About</a> {/* New menu item */}
-      <a href="/profile"><i className="fa fa-fw fa-user"></i> Profile</a> {/* Profile menu item */}
-
-
-    </div>
+    <nav className="navbar">
+      <NavLink to="Homepage.jsx" className="nav-link">
+        <FaHome /> Home
+      </NavLink>
+      <NavLink to="/search" className="nav-link">
+        <FaSearch /> Search
+      </NavLink>
+      <NavLink to="/settings" className="nav-link">
+        <FaCog /> Settings
+      </NavLink>
+      <NavLink to="/about" className="nav-link">
+        <FaInfoCircle /> About
+      </NavLink>
+      <NavLink to="/maps" className="nav-link">
+        <FaMap /> Maps
+      </NavLink>
+      <NavLink to="/profile" className="nav-link">
+        <FaUser /> Profile
+      </NavLink>
+    </nav>
   );
 }
